c21fdeab5e03a696ed1e5abfab7cdf809feefcc6
Sebastian Weigand <sab@sab.systems>
2015-06-01 10:32:56 -0500

    flac2aac - Initial baseline mode POC

--- flac2aac.py
+++ flac2aac.py
@@ -18,7 +18,7 @@ License:        MIT
 Description:    A script which converts FLAC files to MPEG4/AAC files and
                 copies over song metadata, utilizing Apple's CoreAudio
                 framework for better AAC support.
-Version:        2.1.1 (Lossless Support)
+Version:        2.2 (Convert to baseline)
 Requirements:
     OS:         Mac OS X, v10.5+    [afconvert]
     Platform:   Python 2.6+         [multiprocessing]
@@ -43,6 +43,7 @@ try:
 
     from subprocess import call, Popen, PIPE
     from multiprocessing import Pool, cpu_count
+    from scandir import walk
 
 except ImportError, e:
     exit('Error: Unable to import requisite modules: %s' % e)
@@ -61,10 +62,9 @@ for program in ['flac', 'afconvert']:
     if not is_progam_valid(program):
         exit('Error: Unable to execute/find "%s" from your PATH.' % program)
 
-afconvert_help_formats = Popen(['afconvert', '-hf'],
-                               stderr=PIPE).communicate()[1]
-data_formats = [format
-                for format in ['aac', 'aace', 'aacf', 'aach', 'aacl', 'aacp']
+afconvert_help_formats = Popen(['afconvert', '-hf'], stderr=PIPE).communicate()[1]
+
+data_formats = [format for format in ['aac', 'aace', 'aacf', 'aach', 'aacl', 'aacp']
                 if format in afconvert_help_formats]
 
 
@@ -76,36 +76,46 @@ def fix_path(path):
 # =============================================================================
 
 parser = argparse.ArgumentParser(
-    description=
-    'Converts FLAC to MPEG4/AAC via CoreAudio and transfers metadata using Mutagen.',
-    epilog=
-    'Note: Mac OS X v10.5+ is required for HE AAC (aach), and 10.7 is required for HE AAC v2 (aacp).')
+    description='Converts FLAC to MPEG4/AAC via CoreAudio and transfers metadata using Mutagen.',
+    epilog='Note: Mac OS X v10.5+ is required for HE AAC (aach), and 10.7 is required for HE AAC v2 (aacp).')
+
 
 parser.add_argument('location',
                     default=fix_path(os.curdir),
                     type=str,
                     help='the location to search for media files [.]')
+
 parser.add_argument(
     '-l', '--lossless',
     action="store_true",
     default=False,
     help='encode in Apple Lossless (ALAC), overrides codec options [no]')
+
 parser.add_argument('-q', '--quality',
                     type=int,
                     default=75,
                     help='VBR quality, in percent [75]')
+
 parser.add_argument('-c', '--codec',
                     choices=data_formats,
                     default='aac',
                     help='codec to use, if available on your platform [aac]')
+
 parser.add_argument('--abr',
                     action="store_true",
                     default=False,
                     help='use average bitrate, instead of variable [no]')
+
 parser.add_argument('--bitrate',
                     type=int,
                     default=256,
                     help='average bitrate, in KB/s [256]')
+
+parser.add_argument('--baseline',
+                    action="store_true",
+                    default=False,
+                    help='convert both AAC (M4A) and MP3 audio files to AAC with the above quality (ignores --lossless).')
+
 args = parser.parse_args()
 
 # Fix up the arguments:
@@ -141,62 +151,112 @@ for loc in (output_location, tmp_location):
 
 def get_flac_files(location):
     global file_count
-    for path, dirs, files in os.walk(location):
+    for path, dirs, files in walk(location):
         for f in files:
             if f.endswith('.flac') and not f.startswith('.'):
                 print 'Got FLAC file:', f
                 yield os.path.join(path, f)
 
 
-def convert_flac_to_aac(flac_file):
-    try:
-        metadata = mutagen.File(flac_file, easy=True)
+def get_audio_files(location):
+    global file_count
+    for path, dirs, files in walk(location):
+        for f in files:
+            if (f.endswith('.m4a') or f.endswith('.mp3')) and not f.startswith('.'):
+                print 'Got audio file:', f
+                yield os.path.join(path, f)
 
-        validKeys = ['title', 'album', 'artist', 'albumartist', 'date',
-                     'comment', 'description', 'grouping', 'genre',
-                     'copyright', 'albumsort', 'albumartistsort', 'artistsort',
-                     'titlesort', 'composersort', 'tracknumber', 'discnumber']
 
-        # Sometimes extraneous data will be stored within FLAC's metadata, and that
-        # confuses the direct key updating methods of the Easy-branch of Mutagen,
-        # so clean it up here:
+def convert_flac_to_wav(flac_file, wav_file):
+    call(['flac', '-s', '-f', '-d', flac_file, '-o', wav_file])
 
-        for key in metadata.keys():
-            if key not in validKeys:
-                del metadata[key]
 
-        # Keep these in /tmp:
-        wav_file = os.path.join(tmp_location,
-                                os.path.basename(flac_file[:-5])) + '.wav'
+def convert_wav_to_aac(wav_file, m4a_file, lossless=args.lossless):
+    if args.lossless:
+        # TODO: Fix multi-channel audio issues, enable proper conversion:
+        # For 5.1 channel FLAC, cannot use soundcheck, must specify chanel layout:
+        # call(['afconvert', '-f', 'm4af', '-d', 'alac', '-l',  'MPEG_5_1_A', wav_file, m4a_file])
 
-        # Final location is back where we started:
-        m4a_file = os.path.join(output_location,
-                                os.path.basename(flac_file[:-5])) + '.m4a'
+        call(['afconvert', '-f', 'm4af', '-d', 'alac',
+              '--soundcheck-generate', wav_file, m4a_file])
+    else:
+        call(['afconvert', '-f', 'm4af', '-d', args.codec, '-b',
+              str(bitrate), '--src-complexity', 'bats', '-s',
+              str(vbr_mode), '-u', 'vbrq', quality,
+              '--soundcheck-generate', wav_file, m4a_file])
 
-        call(['flac', '-s', '-f', '-d', flac_file, '-o', wav_file])
 
-        if args.lossless:
-            # TODO: Fix multi-channel audio issues, enable proper conversion:
-            # For 5.1 channel FLAC, cannot use soundcheck, must specify chanel layout:
-            # call(['afconvert', '-f', 'm4af', '-d', 'alac', '-l',  'MPEG_5_1_A', wav_file, m4a_file])
+def convert_readable_audio_to_aac(audio_file, m4a_file):
+    call(['afconvert', '-f', 'm4af', '-d', args.codec, '-b',
+          str(bitrate), '--soundcheck-generate', audio_file, m4a_file])
 
-            call(['afconvert', '-f', 'm4af', '-d', 'alac',
-                  '--soundcheck-generate', wav_file, m4a_file])
-        else:
-            call(['afconvert', '-f', 'm4af', '-d', args.codec, '-b',
-                  str(bitrate), '--src-complexity', 'bats', '-s',
-                  str(vbr_mode), '-u', 'vbrq', quality,
-                  '--soundcheck-generate', wav_file, m4a_file])
 
-        m4aData = mutagen.File(m4a_file, easy=True)
-        m4aData.update(metadata)
-        m4aData.save()
-        os.remove(wav_file)
+def transfer_metadata(source_file, target_file):
+    # Open the file with "easy tags" to standardize tagging:
+    metadata = mutagen.File(source_file, easy=True)
+    valid_keys = ['album', 'albumartist', 'albumartistsort', 'albumsort',
+                  'artist', 'artistsort', 'APIC:'
+                  'comment', 'composersort', 'covr',
+                  'copyright',
+                  'date',
+                  'description',
+                  'discnumber',
+                  'genre',
+                  'grouping',
+                  'musicbrainz_albumartistid',
+                  'musicbrainz_albumid',
+                  'musicbrainz_albumstatus',
+                  'musicbrainz_albumtype',
+                  'musicbrainz_artistid',
+                  'musicbrainz_trackid',
+                  'pictures',
+                  'title',
+                  'titlesort',
+                  'tracknumber']
 
-        print 'Finished file:', m4a_file
+    for key in metadata.keys():
+        if key not in valid_keys:
+            del metadata[key]
 
-    except KeyboardInterrupt:
-        pass
+    m4a_data = mutagen.File(target_file, easy=True)
+    m4a_data.update(metadata)
+    m4a_data.save()
+
+    # Open the file again with extended metadata for album art:
+    additional_metadata = mutagen.File(source_file, easy=False)
+    m4a_data = mutagen.File(target_file, easy=False)
+
+    if type(additional_metadata) == mutagen.flac.FLAC:
+        m4a_data['covr'] = [mutagen.mp4.MP4Cover(pic.data, mutagen.mp4.MP4Cover.FORMAT_JPEG) if 'jpeg' in pic.mime else mutagen.mp4.MP4Cover(pic.data, mutagen.mp4.MP4Cover.FORMAT_PNG) for pic in additional_metadata.pictures]
+
+    elif type(additional_metadata) == mutagen.mp3.MP3:
+        m4a_data['covr'] = [mutagen.mp4.MP4Cover(additional_metadata['APIC:'].data, mutagen.mp4.MP4Cover.FORMAT_JPEG if 'jpeg' in additional_metadata['APIC:'].mime else mutagen.mp4.MP4Cover.FORMAT_PNG)]
+
+    elif type(additional_metadata) == mutagen.mp4.MP4:
+        m4a_data['covr'] = additional_metadata['covr']
+
+    m4a_data.save()
+
+
+def process_flac_file(flac_file):
+    wav_file = os.path.join(tmp_location, os.path.basename(flac_file[:-5])) + '.wav'
+    m4a_file = os.path.join(output_location, os.path.basename(flac_file[:-5])) + '.m4a'
+
+    convert_flac_to_wav(flac_file, wav_file)
+    convert_wav_to_aac(wav_file, m4a_file)
+    transfer_metadata(flac_file, m4a_file)
+    os.remove(wav_file)
+
+    print 'Finished file:', m4a_file
+
+
+def process_audio_file(audio_file):
+    m4a_file = os.path.join(output_location, os.path.basename(audio_file[:-4])) + '.m4a'
+    convert_readable_audio_to_aac(audio_file, m4a_file)
+    transfer_metadata(audio_file, m4a_file)
+
+    print 'Finished file:', m4a_file
+
 
 # =============================================================================
 # Main:
@@ -206,7 +266,11 @@ print 'Processing: %s...' % args.location
 
 try:
     pool = Pool()
-    p = pool.map_async(convert_flac_to_aac, get_flac_files(args.location))
+
+    if args.baseline:
+        p = pool.map_async(process_audio_file, get_audio_files(args.location))
+    else:
+        p = pool.map_async(process_flac_file, get_flac_files(args.location))
     p.get(0xFFFF)
 
 except KeyboardInterrupt:
